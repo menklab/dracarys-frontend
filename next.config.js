ca0fce1509903c6ece320dee95adcb8a6421adc8
shimevaily <ilia.krasa@menklab.com>
2023-01-06 18:36:35 +0200

    updated env

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
-  env: {
-    NEST_HOST: process.env.NEST_HOST, // TO BE ABLE TO USE ON CLIENT SIDE
-    NEXT_HOST: process.env.NEXT_HOST,
-  },
 };
 
 module.exports = nextConfig;
