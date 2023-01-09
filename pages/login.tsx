72d71194701b64d85127cb2b650d2f8bba5755a6
MizhgunMenk <michael.gaydey@menklab.com>
2023-01-09 18:05:16 +0200

    for check

--- pages/login.tsx
+++ pages/login.tsx
@@ -14,7 +14,8 @@ export default function Login() {
       <Stack direction="column" justifyContent="center" alignItems="center" spacing={2} sx={{ height: "100vh" }}>
         <Typography variant="h3">Login</Typography>
         <Typography variant="body1" sx={{ maxWidth: 380, textAlign: "center" }}>
-          By logging in you are agreeing to the manufacterers <br /> Privacy Policy and Terms of use.
+          By logging in you are agreeing to the manufacterers <br /> Privacy Policy and Terms of use. **
+          {process.env.NEST_HOST}**
         </Typography>
         <Button variant="contained" onClick={connectToPhantom}>
           Login
