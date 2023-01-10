b28fd6a70bf6bbd207d565472568672a810aeb3b
MizhgunMenk <michael.gaydey@menklab.com>
2023-01-10 09:58:23 +0200

    fix env problem

--- pages/login.tsx
+++ pages/login.tsx
@@ -9,15 +9,12 @@ export default function Login() {
     actions: { connectToPhantom },
   } = useAuth();
 
-  console.log(process.env);
-
   return (
     <Container maxWidth="lg">
       <Stack direction="column" justifyContent="center" alignItems="center" spacing={2} sx={{ height: "100vh" }}>
         <Typography variant="h3">Login</Typography>
         <Typography variant="body1" sx={{ maxWidth: 380, textAlign: "center" }}>
-          By logging in you are agreeing to the manufacterers <br /> Privacy Policy and Terms of use. **
-          {process.env.NEST_HOST}** DOPPLER_ENVIRONMENT - {process.env.DOPPLER_ENVIRONMENT}**
+          By logging in you are agreeing to the manufacterers <br /> Privacy Policy and Terms of use.
         </Typography>
         <Button variant="contained" onClick={connectToPhantom}>
           Login
