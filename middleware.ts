7793bbe41bfa0da33f98c1a18f0cba9d5b481710
MizhgunMenk <michael.gaydey@menklab.com>
2023-01-19 16:31:32 +0200

    test deploy

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { ROUTES } from "~/constants/routes";
 
+// for test
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const authSid = request.cookies.get("connect.sid")?.value;
