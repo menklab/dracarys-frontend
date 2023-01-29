06d95d7613e5ee5ec0c27c21aee8a0e8f611a2a9
shimevaily <105929198+shimevaily@users.noreply.github.com>
2023-01-29 22:22:42 +0200

    create program name trim (#21)

--- forms/createProgram.ts
+++ forms/createProgram.ts
@@ -4,6 +4,7 @@ import { object, string, TypeOf } from "zod";
 
 export const createProgramSchema = object({
   name: string({ required_error: "Name is required", invalid_type_error: "Name must be a string" })
+    .trim()
     .min(1, "Name must be between 1 and 50 characters long")
     .max(50, "Name must be between 1 and 50 characters long"),
 });
