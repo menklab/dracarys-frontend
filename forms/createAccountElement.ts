97550aa24d0395e6e9b04b3c5f36afdf5a428b1f
MizhgunMenk <michael.gaydey@menklab.com>
2023-02-01 18:14:34 +0200

    new handler to create account element and type validation

--- forms/createAccountElement.ts
+++ forms/createAccountElement.ts
@@ -1,14 +1,18 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { DefaultValues, useForm } from "react-hook-form";
-import { enum as zodEnum, object, string, TypeOf } from "zod";
-import { TypeArrayValidation } from "~/enums/elementType";
+import { nativeEnum, object, string, TypeOf, ZodErrorMap } from "zod";
+import { ElementType } from "~/enums/elementType";
+
+const errorMap: ZodErrorMap = () => {
+  return { message: "Account Type is required" };
+};
 
 export const createAccountElementSchema = object({
   name: string({ required_error: "Name is required", invalid_type_error: "Name must be a string" })
     .trim()
     .min(1, "Name must be between 1 and 50 characters long")
     .max(50, "Name must be between 1 and 50 characters long"),
-  type: zodEnum(TypeArrayValidation),
+  type: nativeEnum(ElementType, { errorMap: errorMap }),
 });
 
 export type CreateAccountElementSchemaType = TypeOf<typeof createAccountElementSchema>;
