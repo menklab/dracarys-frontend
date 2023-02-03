448d878b3847161bac16058f4824cc1737c8e157
MizhgunMenk <120572101+MizhgunMenk@users.noreply.github.com>
2023-02-03 17:31:01 +0200

    fix validation account name (#31)

--- forms/editAccountName.ts
+++ forms/editAccountName.ts
@@ -6,11 +6,16 @@ export const editAccountNameSchema = object({
   name: string({ required_error: "Name is required", invalid_type_error: "Name must be a string" })
     .trim()
     .min(1, "Name must be between 1 and 50 characters long")
-    .max(50, "Name must be between 1 and 50 characters long"),
+    .max(50, "Name must be between 1 and 50 characters long")
+    .regex(/^[A-Za-z0-9]+$/, "Field can only include letters and numbers"),
 });
 
 export type EditAccountNameSchemaType = TypeOf<typeof editAccountNameSchema>;
 
 export const useEditAccountNameForm = (defaultValues: DefaultValues<EditAccountNameSchemaType>) => {
-  return useForm<EditAccountNameSchemaType>({ resolver: zodResolver(editAccountNameSchema), defaultValues });
+  return useForm<EditAccountNameSchemaType>({
+    mode: "all",
+    resolver: zodResolver(editAccountNameSchema),
+    defaultValues,
+  });
 };
