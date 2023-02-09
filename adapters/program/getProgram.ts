1a2f3e846585663fbccf485c697dea3b3adb7e72
shimevaily <ilia.krasa@menklab.com>
2023-02-09 19:35:43 +0200

    program interface and adapters changes

--- adapters/program/getProgram.ts
+++ adapters/program/getProgram.ts
@@ -1,11 +1,26 @@
 import { API_ROUTES } from "~/constants/api_routes";
 import { Program } from "~/interfaces/program";
 
-type GetProgramJsonResponse = { id: number; name: string; createdAt: string; updatedAt: string };
+type GetProgramJsonResponse = {
+  id: number;
+  name: string;
+  center?: [number, number];
+  zoom?: number;
+  createdAt: string;
+  updatedAt: string;
+};
 
 // SSR usage only
 export default async function getProgram(sid: string, programId: number): Promise<Program> {
   const res = await fetch(API_ROUTES.PROGRAM(programId), { method: "GET", headers: { cookie: `connect.sid=${sid}` } });
   if (!res.ok) throw await res.json();
-  return { ...((await res.json()) as GetProgramJsonResponse) };
+  const data = (await res.json()) as GetProgramJsonResponse;
+  return {
+    id: data.id,
+    name: data.name,
+    ...(data?.center ? { center: { x: data.center[0], y: data.center[1] } } : {}),
+    ...(data?.zoom ? { zoom: data.zoom } : {}),
+    createdAt: data.createdAt,
+    updatedAt: data.updatedAt,
+  };
 }
--- adapters/program/updateProgram.ts
+++ adapters/program/updateProgram.ts
@@ -1,13 +1,21 @@
 import { API_ROUTES } from "~/constants/api_routes";
+import { Position } from "~/interfaces/position";
 
 interface UpdateProgramBody {
   name: string;
+  center?: Position;
+  zoom?: number;
 }
 
 // FE usage only
 export default async function updateProgram(programId: number, body: UpdateProgramBody): Promise<void> {
+  const { name, center, zoom } = body;
   const res = await fetch(API_ROUTES.PROGRAM(programId), {
-    body: JSON.stringify(body),
+    body: JSON.stringify({
+      name,
+      ...(center ? { center: [center.x, center.y] } : { center: [0, 0] }),
+      ...(zoom ? { zoom } : { zoom: 1 }),
+    }),
     method: "PATCH",
     headers: { "Content-Type": "application/json" },
     credentials: "include",
