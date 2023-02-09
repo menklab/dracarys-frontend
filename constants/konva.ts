6f8f2086019850342f1d21210cf815d7ad9060e1
shimevaily <ilia.krasa@menklab.com>
2023-02-09 19:36:10 +0200

    new konva utility functions and constants

--- constants/konva.ts
+++ constants/konva.ts
@@ -1,3 +1,5 @@
+import { Position } from "~/interfaces/position";
+
 export const KONVA_ACCOUNT_WIDTH = 200;
 export const KONVA_ACCOUNT_STROKE_WIDTH = 2;
 export const KONVA_ACCOUNT_STROKE_COLOR = "black";
@@ -10,6 +12,7 @@ export const KONVA_ACCOUNT_CROWN_OFFSET_X = 5;
 export const KONVA_ACCOUNT_CROWN_OFFSET_Y = 5;
 export const KONVA_ACCOUNT_CROWN_SHADOW_BLUR = 4;
 export const KONVA_ACCOUNT_CROWN_STROKE = "black";
+export const KONVA_ACCOUNT_DEFAULT_POSITION: Position = { x: 10, y: 10 };
 export const KONVA_ACCOUNT_NAME_FONT_SIZE = 16;
 export const KONVA_ACCOUNT_ATTRIBUTE_HEIGHT = 20;
 export const KONVA_ACCOUNT_ATTRIBUTE_FONT_SIZE = 12;
@@ -22,3 +25,5 @@ export const KONVA_ACCOUNT_RECT_MAKER_ID = "account-rect";
 export const KONVA_ACCOUNT_CROWN_MAKER_ID = "account-crown";
 export const KONVA_ACCOUNT_ATTRIBUTES_MAKER_ID = "account-attributes";
 export const KONVA_CONNECTION_MAKER_ID = "connection";
+export const KONVA_DEFAULT_STAGE_POSITION: Position = { x: 0, y: 0 };
+export const KONVA_DEFAULT_STAGE_SCALE = 1;
