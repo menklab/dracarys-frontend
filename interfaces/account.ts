7b5b50f18bf0a3fb62d4cc2071174c3963b61443
shimevaily <ilia.krasa@menklab.com>
2023-02-09 19:35:10 +0200

    removed unused attributes

--- interfaces/account.ts
+++ interfaces/account.ts
@@ -1,10 +1,8 @@
-import { Attribute } from "~/interfaces/attribute";
 import { Position } from "~/interfaces/position";
 
 export interface Account {
   id: number;
   name: string;
-  attributes?: Attribute[];
   linkedAccounts: number[];
   position?: Position;
   createdAt?: string;
